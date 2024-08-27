dc4e31247739e8f12be77213cbd61fcf23ec1c47
Ayuna <lsvwu12@gmail.com>
2024-08-28 00:50:36 +0300

    Update to double version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,27 @@
 package org.example;
 
 import java.io.*;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        long startTime = System.currentTimeMillis();
+        Long startTime = System.currentTimeMillis();
         if (args.length < 1) {
             System.out.println("Не указано имя файла с исходными данными!");
             return;
         }
 
         // read all data and process it
-        List<List<Long>> lines = new ArrayList<>();
+        List<List<Double>> lines = new ArrayList<>();
+
         readAndProcessData(args[0], lines);
 //        logTimeFromStart(startTime, "End of reading and processing data!");
 
         // get all information about same values in the same columns but from different lines
-        List<Map<Long, List<Integer>>> corrTable = new ArrayList<>();
-        List<Integer> lineGroups = new ArrayList<>(lines.size());
+        List<Map<Integer, List<Integer>>> corrTable = new ArrayList<>();
+        List<Integer> lineGroups = new ArrayList<>();
         getConnections(lines, lineGroups, corrTable);
 //        logTimeFromStart(startTime, "End of parsing data and making table of connections!");
 
@@ -34,21 +37,21 @@ public class Main {
         logTimeFromStart(startTime, "End of program");
     }
 
-    public static void logTimeFromStart(long startTime, String event) {
+    public static void logTimeFromStart(Long startTime, String event) {
 //        System.out.println(event);
-        long endTime = System.currentTimeMillis();
-        System.out.println("That took " + (endTime - startTime) + " milliseconds");
+        Long endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime - startTime) + " ms.");
     }
 
-    public static void readAndProcessData(String inputFileName, List<List<Long>> lines) {
+    public static void readAndProcessData(String inputFileName, List<List<Double>> lines) {
         // create reader
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFileName))) {
             String line;
-            Set<Integer> setOfLinesHashCodes = new HashSet<>();
+            Set<String> setOfLines = new HashSet<>();
             // read line
             while ((line = reader.readLine()) != null) {
                 // parse and write data in lines list
-                processLine(line, lines, setOfLinesHashCodes);
+                processLine(line, lines, setOfLines);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
@@ -56,19 +59,18 @@ public class Main {
         }
     }
 
-    public static void processLine(String line, List<List<Long>> lines, Set<Integer> setOfLines) {
-        // get hashcode of line and check if the same line was before
+    public static void processLine(String line, List<List<Double>> lines, Set<String> setOfLines) {
+        // check if the same line was before
         // this check could be more complex in case of different space symbols and any other problems
         //  line.matches(".*\\d.*") ->
         //      this for the way to not skip lines with empty values - as each line of this type should be in its own group
-        Integer lineHashCode = line.hashCode();
-        if (setOfLines.contains(lineHashCode) && line.matches(".*\\d.*")) {
+        if (line.isBlank()) { return; }
+        if (setOfLines.contains(line) && line.matches(".*\\d.*")) {
             return;
         }
-
         // split elements by ";"
-        List<String> elements = new ArrayList<>(Arrays.stream(line.split(";")).toList());
-        List<Long> parsedElements = new ArrayList<>(elements.size());
+        List<String> elements = new ArrayList<>(Arrays.stream(line.split(";", -1)).toList());
+        List<Double> parsedElements = new ArrayList<>(elements.size());
         if (elements.isEmpty()) {
             return;
         }
@@ -76,8 +78,12 @@ public class Main {
         for (int i = 0; i < elements.size(); i++) {
             String element = elements.get(i);
             // check if element is in right format
-            //  (regular expression was made on the basis of lng.txt)
-            if (!element.matches("\"\\d*\"")) {
+            //  (regular expression was made on the basis of lng-big.csv)
+            if (element.isBlank()) {
+                parsedElements.add(null);
+                continue;
+            }
+            if (!element.matches("\"[0-9]{0,13}(\\.[0-9]*)?\"")) {
                 return;
             } else {
                 // parse element and add to list
@@ -85,24 +91,32 @@ public class Main {
                 if (elWithoutQuotes.isBlank()) {
                     parsedElements.add(null);
                 } else {
-                    parsedElements.add(i, Long.parseLong(elWithoutQuotes));
+                    parsedElements.add(i, Double.parseDouble(elWithoutQuotes));
                 }
             }
         }
+
         // add line hash code to set
-        setOfLines.add(lineHashCode);
+        setOfLines.add(line);
         // add parsed line to list
         lines.add(parsedElements);
     }
 
-    public static void getConnections(List<List<Long>> lines, List<Integer> lineGroups,
-                                      List<Map<Long, List<Integer>>> corrTable) {
+    public static void getConnections(List<List<Double>> lines, List<Integer> lineGroups,
+                                      List<Map<Integer, List<Integer>>> corrTable) {
         for (int i = 0; i < lines.size(); i++) {
+            getConnection(i, lines, lineGroups, corrTable);
+        }
+    }
+
+    public static void getConnection(int i, List<List<Double>> lines, List<Integer> lineGroups,
+                                      List<Map<Integer, List<Integer>>> corrTable) {
+            DecimalFormat format = new DecimalFormat("#.#####");
             // initialize group for every line with default value: -1
             lineGroups.add(-1);
             // for every column collect unique values and mark in which lines they are
             for (int j = 0; j < lines.get(i).size(); j++) {
-                Long element = lines.get(i).get(j);
+                Double element = lines.get(i).get(j);
                 // initialize map for column
                 if (corrTable.size() < j + 1) {
                     corrTable.add(new HashMap<>());
@@ -112,19 +126,20 @@ public class Main {
                     continue;
                 }
                 // put element and number of line to corresponding map element
-                Map<Long, List<Integer>> column = corrTable.get(j);
-                if (!column.containsKey(element)) {
-                    column.put(element, new ArrayList<>(List.of(i)));
+                Map<Integer, List<Integer>> column = corrTable.get(j);
+                Integer key = format.format(element).hashCode();
+                if (!column.containsKey(key)) {
+                    column.put(key, new ArrayList<>(List.of(i)));
                 } else {
-                    List<Integer> columnGroup = column.get(element);
+                    List<Integer> columnGroup = column.get(key);
                     columnGroup.add(i);
                 }
             }
-        }
+
     }
 
-    public static void findAllGroups(List<List<Long>> lines, List<Integer> lineGroups,
-                                     List<Set<Integer>> groups, List<Map<Long, List<Integer>>> corrTable) {
+    public static void findAllGroups(List<List<Double>> lines, List<Integer> lineGroups,
+                                     List<Set<Integer>> groups, List<Map<Integer, List<Integer>>> corrTable) {
         // for every line:
         for (int i = 0; i < lines.size(); i++) {
             // skip if line is already in any group
@@ -151,30 +166,46 @@ public class Main {
 
     public static void addLinesToGroup(Integer curLine, Integer groupNumber,
                                        Queue<Integer> linesToCheck, Set<Integer> currentGroup,
-                                       List<List<Long>> lines, List<Integer> lineGroups,
-                                       List<Map<Long, List<Integer>>> corrTable) {
-        List<Long> line = lines.get(curLine);
+                                       List<List<Double>> lines, List<Integer> lineGroups,
+                                       List<Map<Integer, List<Integer>>> corrTable) {
+        DecimalFormat format = new DecimalFormat("#.#####");
+        List<Double> line = lines.get(curLine);
         // for every element of line:
         for (int j = 0; j < line.size(); j++) {
-            Long element = line.get(j);
+            Double element = line.get(j);
+            if (element == null) {
+                continue;
+            }
+            Integer key = format.format(element).hashCode();
             // get connections to other lines
-            Map<Long, List<Integer>> column = corrTable.get(j);
-            List<Integer> linesToAdd = column.get(element);
+            Map<Integer, List<Integer>> column = corrTable.get(j);
+            List<Integer> linesToAdd = column.get(key);
+            List<Integer> newColumnValues = new ArrayList<>();
+
             if (linesToAdd == null) {
                 continue;
             }
             // add connected line to group and to queue
             //  if group did not already contain this line in it
             for (Integer lineToAdd : linesToAdd) {
-                if (currentGroup.add(lineToAdd)) {
-                    linesToCheck.add(lineToAdd);
-                    lineGroups.set(lineToAdd, groupNumber);
+                if (!currentGroup.contains(lineToAdd)) {
+                    if (compare(lines.get(lineToAdd).get(j), element) == 0) {
+                        currentGroup.add(lineToAdd);
+                        linesToCheck.add(lineToAdd);
+                        lineGroups.set(lineToAdd, groupNumber);
+                    } else {
+                        newColumnValues.add(lineToAdd);
+                    }
                 }
             }
+            column.put(key, newColumnValues);
         }
     }
 
-    public static void writeResultsToFile(String outputFileName, List<List<Long>> lines, List<Set<Integer>> groups) {
+    public static void writeResultsToFile(String outputFileName, List<List<Double>> lines, List<Set<Integer>> groups) {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setDecimalSeparator('.');
+        DecimalFormat format = new DecimalFormat("#.#####", symbols);
         // create writer
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
             // write number of groups with more than one element
@@ -190,14 +221,14 @@ public class Main {
                 writer.write("Группа № " + (groups.size() - groupNumber));
                 writer.newLine();
                 for (int lineNumber : groups.get(groupNumber)) {
-                    List<Long> line = lines.get(lineNumber);
+                    List<Double> line = lines.get(lineNumber);
                     for (int i = 0; i < line.size(); i++) {
-                        Long el = line.get(i);
+                        Double el = line.get(i);
                         if (el != null) {
-                            writer.write("\"" + el + "\"");
-                        } else {
+                            writer.write("\"" + format.format(el) + "\"");
+                        } /*else {
                             writer.write("\"\"");
-                        }
+                        }*/
                         if (i < line.size() - 1) {
                             writer.write(";");
                         }
@@ -210,4 +241,18 @@ public class Main {
             System.exit(1);
         }
     }
+
+//    public static class DoubleComparator implements Comparator<Double> {
+
+        public static int compare(Double o1, Double o2) {
+            double delta = o1 - o2;
+            if (Math.abs(delta) < 1E-5) {
+                return 0;
+            }
+            if (delta < 0) {
+                return -1;
+            }
+            return 1;
+        }
+//    }
 }
